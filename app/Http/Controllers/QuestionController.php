82af3c180b35e4a4fa281aeb01c0469bc99717ed
Fernando Moreno <rasecfer@gmail.com>
2019-01-28 23:41:27 -0600

    Question CRUD

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Question;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Http\Resources\QuestionResource;
 
 class QuestionController extends Controller
 {
@@ -14,17 +16,7 @@ class QuestionController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        return QuestionResource::collection(Question::latest()->get());
     }
 
     /**
@@ -35,7 +27,10 @@ class QuestionController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+        // auth()->user()->question()->create($request->all());
+        $question = Question::create($request->all());
+        return $question;
     }
 
     /**
@@ -46,7 +41,7 @@ class QuestionController extends Controller
      */
     public function show(Question $question)
     {
-        //
+        return new QuestionResource($question);
     }
 
     /**
@@ -69,7 +64,8 @@ class QuestionController extends Controller
      */
     public function update(Request $request, Question $question)
     {
-        //
+        $question->update($request->all());
+        return $question;
     }
 
     /**
@@ -80,6 +76,7 @@ class QuestionController extends Controller
      */
     public function destroy(Question $question)
     {
-        //
+        $question->delete();
+        return response('Deleted', Response::HTTP_NO_CONTENT);
     }
 }
--- app/Question.php
+++ app/Question.php
@@ -6,6 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
+    
+    protected $table = 'questions';
+
+    protected $fillable = [
+        'title', 'slug', 'body', 'category_id', 'user_id'
+    ];
+
+    /**
+     * Con esta función se le indica a Laravel cuál es el campo parámetro de búsqueda en la ruta
+     * En este caso el campo será slug
+     */
+    public function getRouteKeyName(){
+        return 'slug';
+    }
+
     //Definición de las relaciones
     public function user(){
         return $this->belongsTo(User::class);
@@ -17,5 +32,9 @@ class Question extends Model
 
     public function category(){
         return $this->belongsTo(Category::class);
+    }
+
+    public function getPathAttribute(){
+        return asset("api/question/$this->slug");
     }
 }
